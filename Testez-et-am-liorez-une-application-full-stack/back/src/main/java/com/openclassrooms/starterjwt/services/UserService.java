97cb0f0e57e286dbd6334f824b24647b435203cb
rouzic pierre <cizuor83@free.fr>
2025-12-11 09:26:50 +0100

    ajout des fonction userService et appel de ses fonction epuis authController

--- Testez-et-am-liorez-une-application-full-stack/back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
+++ Testez-et-am-liorez-une-application-full-stack/back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
@@ -2,6 +2,9 @@ package com.openclassrooms.starterjwt.services;
 
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +21,18 @@ public class UserService {
 
     public User findById(Long id) {
         return this.userRepository.findById(id).orElse(null);
+    }
+
+    public Optional<User> findByEmail(String email){
+        return this.userRepository.findByEmail(email);
+    }
+
+    public boolean existsByEmail(String email){
+        return this.userRepository.existsByEmail(email);
+    }
+
+
+    public User saveUser(User user){
+        return this.userRepository.save(user);
     }
 }
