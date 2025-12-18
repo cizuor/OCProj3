79131020144aac30d83af52e0c5160268a27b32a
rouzic pierre <cizuor83@free.fr>
2025-12-18 09:50:19 +0100

    ajout des test Jtest sur register

--- Testez-et-am-liorez-une-application-full-stack/front/src/app/pages/register/register.component.spec.ts
+++ Testez-et-am-liorez-une-application-full-stack/front/src/app/pages/register/register.component.spec.ts
@@ -7,17 +7,35 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
+import { of, throwError } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { Router } from '@angular/router';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  const mockAuthService = {
+    register: jest.fn()
+  };
+
+  const mockRouter = {
+    navigate: jest.fn()
+  };
+
+  const validRegisterForm = {
+    email: 'test@test.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    password: 'password123'
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RegisterComponent],
       imports: [
+        RegisterComponent,
         BrowserAnimationsModule,
         HttpClientModule,
         ReactiveFormsModule,  
@@ -25,6 +43,10 @@ describe('RegisterComponent', () => {
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
+      ],
+      providers:[
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter }
       ]
     })
       .compileComponents();
@@ -34,7 +56,37 @@ describe('RegisterComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should initialize with invalid empty form', () => {
+    expect(component.form.valid).toBeFalsy();
+    expect(component.form.get('email')?.value).toBe('');
+    expect(component.onError).toBe(false);
+  });
+
+  it('should call authService.register and navigate to login on success', () => {
+    mockAuthService.register.mockReturnValue(of(void 0));
+    component.form.setValue(validRegisterForm);
+    component.submit();
+    expect(mockAuthService.register).toHaveBeenCalledWith(validRegisterForm);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/login']);
+    expect(component.onError).toBe(false);
+  });
+
+  it('should set onError to true on registration failure', () => {
+    mockAuthService.register.mockReturnValue(throwError(() => new Error('Sign up failed')));
+    component.form.setValue(validRegisterForm);
+    component.submit();
+    expect(mockAuthService.register).toHaveBeenCalledWith(validRegisterForm);
+    expect(component.onError).toBe(true); // Le flag d'erreur doit être activé
+    expect(mockRouter.navigate).not.toHaveBeenCalled(); // On ne doit pas changer de page
   });
 });
